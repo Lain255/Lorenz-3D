a07c940dee4db26a74dcee0e6ea415e7d247c319
Lain <65585093+Lain69@users.noreply.github.com>
2022-01-26 18:49:39 -0600

    Massive code refactoring
    Removed several files and somehow main is shorter.

--- main.c
+++ main.c
@@ -1,264 +1,188 @@
-#include "camera.h"
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct V3_s {
+    double x;
+    double y;
+    double z;
+} V3;
+
+V3 add(V3 a, V3 b) {
+    V3 r = {a.x + b.x, a.y + b.y, a.z + b.z}; 
+    return r;
+}
+
+V3 mult(double c, V3 a) {
+    V3 r = {c* a.x, c * a.y, c * a.z}; 
+    return r;
+}
+
+V3 randV3() {
+    V3 r = {
+        (rand() % 100000)/100000.0, 
+        (rand() % 100000)/100000.0,
+        (rand() % 100000)/100000.0
+    };
+    return r;
+}
 
 const double PI = 3.1415926;
-const int fps = 60;
-double dt = 1. / 60; //1/fps
-double timeSlow = 1;
-double A = 10;
-double O = 28;
-double C = 8 / 3.;
-double sourceSize = 0.5;
-int numParticles = 30000;
-int ctrlDown = 1;
-bool paused = false;
-bool flyFree = false;
+const int numParticles = 100000;
+double A = 10.0;
+double B = 8.0 / 3.0;
+double C = 28.0;
+double dt = 1.0 / 60.0;
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  //loads the number of points
-  FILE* numParticlesFile = fopen("numParticles.txt", "r");
-  fscanf(numParticlesFile, "%i", &numParticles);
-  fclose(numParticlesFile);
+    SDL_Init(SDL_INIT_VIDEO);
 
-  Camera c;
-  if (initCamera(&c))
-  {
-    //Main loop flag
-    bool quit = false;
+    int w = 800;
+    int h = 600;
+    double POV = 2;
 
-    //create and initialize particles
-    Particle *particleList = malloc(numParticles * sizeof(Particle));
+    SDL_Window *window = SDL_CreateWindow(
+        "Lorenze", 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        w, 
+        h,
+        SDL_WINDOW_MAXIMIZED
+    );
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED || SDL_RENDERER_PRESENTVSYNC
+    );
+
+    V3 particals[numParticles];
+    SDL_FPoint points[numParticles];
+    V3 c = {0,0,0};
+    double theta = 0;
+    double phi = 0;
+    int dTheta, dPhi;
+    double cosTheta = cos(theta);
+    double sinTheta = sin(theta);
+    double cosPhi = cos(phi);
+    double sinPhi = sin(phi);
+
+    int stopTime = 1;
+    int turbo;
+
     for (int i = 0; i < numParticles; i++)
     {
-      spawnParticle(&particleList[i]);
+        particals[i] = randV3();
     }
     
-    //Event handler
+    SDL_bool quit = SDL_FALSE;
     SDL_Event e;
     SDL_SetRelativeMouseMode(SDL_TRUE);
+    const Uint8* keyboard = SDL_GetKeyboardState(NULL);
 
-    int startOfFrame = SDL_GetTicks();
-    while (!quit)
+    while (!quit) 
     {
-      //handles events on queue
-      while (SDL_PollEvent(&e) != 0)
-      {
-        if (e.type == SDL_QUIT)
-        {
-          quit = true;
-        }
-        else if (e.type == SDL_KEYDOWN)
-        {
-          switch (e.key.keysym.sym)
-          {
-          //quiting
-          case SDLK_ESCAPE:
-            quit = true;
-            break;
+        while (SDL_PollEvent(&e)) {
+            switch(e.type) {
+            
+            case SDL_QUIT:
+                quit = SDL_TRUE;    
+                break;
+                
+            case SDL_KEYDOWN:
+                if (e.key.keysym.scancode == SDL_SCANCODE_R)
+                {
+                    A = 10.0;
+                    B = 8.0 / 3.0;
+                    C = 28.0;
+                }
+                
+                if (e.key.keysym.scancode == SDL_SCANCODE_F)
+                    stopTime = !stopTime;
+                break;
+            
+            case SDL_MOUSEMOTION:
+                SDL_GetRelativeMouseState(&dTheta, &dPhi);
+                theta += dTheta / (100 * PI);
+                phi -= dPhi / (100 * PI);
 
-          //absolute movements
-          case SDLK_s:
-            c.v.z = -10 * ctrlDown;
-            break;
-          case SDLK_w:
-            c.v.z = 10 * ctrlDown;
-            break;
-          case SDLK_a:
-            c.v.x = -10 * ctrlDown;
-            break;
-          case SDLK_d:
-            c.v.x = 10 * ctrlDown;
-            break;
-          case SDLK_q:
-            c.v.y = -10 * ctrlDown;
-            break;
-          case SDLK_e:
-            c.v.y = 10 * ctrlDown;
-            break;
+                if (phi > PI / 2)
+                {
+                    phi = PI / 2;
+                }
+                if (phi < -PI / 2)
+                {
+                    phi = -PI / 2;
+                }
+                
+                cosTheta = cos(theta);
+                sinTheta = sin(theta);
+                cosPhi = cos(phi);
+                sinPhi = sin(phi);
 
-          //editing a,o,c coeffecients
-          case SDLK_u:
-            A += 0.1 * ctrlDown;
-            break;
-          case SDLK_j:
-            A += -0.1 * ctrlDown;
-            break;
-          case SDLK_i:
-            O += 0.1 * ctrlDown;
-            break;
-          case SDLK_k:
-            O += -0.1 * ctrlDown;
-            break;
-          case SDLK_o:
-            C += 0.1 * ctrlDown;
-            break;
-          case SDLK_l:
-            C += -0.1 * ctrlDown;
-            break;
-
-          //edit time
-          case SDLK_t:
-            timeSlow *= 0.95;
-            break;
-          case SDLK_g:
-            timeSlow *= 1.05;
-            break;
-          case SDLK_p:
-            paused = !paused;
-            break;
-
-          //fly free mode
-          case SDLK_f:
-            flyFree = !flyFree;
-            break;
-
-          //reset variables
-          case SDLK_r:
-            if (ctrlDown != 1)
-            {
-              for (int i = 0; i < numParticles; i++)
-              {
-                spawnParticle(&particleList[i]);
-              }
+                break;
             }
-            else
-            {
-              timeSlow = 1;
-              A = 10;
-              O = 28;
-              C = 8 / 3.;
-              resetCamera(&c);
-            }
-            break;
+        }
 
-          //holding down control
-          case SDLK_LCTRL:
-          case SDLK_RCTRL:
-            ctrlDown = 5;
-            break;
-          }
-        }
-        if (e.type == SDL_KEYUP)
-        {
-          switch (e.key.keysym.sym)
-          {
-          //stop absolute movement
-          case SDLK_s:
-          case SDLK_w:
-            c.v.z = 0;
-            break;
-          case SDLK_a:
-          case SDLK_d:
-            c.v.x = 0;
-            break;
-          case SDLK_q:
-          case SDLK_e:
-            c.v.y = 0;
-            break;
+        quit = quit || keyboard[SDL_SCANCODE_ESCAPE];
+        turbo = 1 + 4*keyboard[SDL_SCANCODE_LSHIFT];
+        
+        double right = 0.1*turbo*(keyboard[SDL_SCANCODE_D] - keyboard[SDL_SCANCODE_A]);
+        double up = 0.1*turbo*(keyboard[SDL_SCANCODE_E] - keyboard[SDL_SCANCODE_Q]);
+        double forward = 0.1*turbo*(keyboard[SDL_SCANCODE_W] - keyboard[SDL_SCANCODE_S]);
 
-          //letting go of control
-          case SDLK_LCTRL:
-          case SDLK_RCTRL:
-            ctrlDown = 1;
-            break;
-          }
-        }
-        else if (e.type == SDL_MOUSEWHEEL)
-        {
-          c.v = sphereToParticle(ctrlDown * 10 * e.wheel.y, c.theta, c.phi);
-        }
-        else if (e.type == SDL_MOUSEBUTTONDOWN)
-        {
-          if (e.button.button == SDL_BUTTON_LEFT)
-          {
-            c.v = sphereToParticle(ctrlDown * 10, c.theta, c.phi);
-          }
-          if (e.button.button == SDL_BUTTON_RIGHT)
-          {
-            c.v = sphereToParticle(-ctrlDown * 10, c.theta, c.phi);
-          }
+        c.x += (forward*cosPhi*sinTheta) + (-up*sinPhi*sinTheta) + (right*cosTheta);    
+        c.y += (forward*sinPhi) + (up*cosPhi);
+        c.z += (forward*cosPhi*cosTheta) + (-up*sinPhi*cosTheta) + (-right*sinTheta);
+        
+        A += 0.1*turbo*(keyboard[SDL_SCANCODE_U] - keyboard[SDL_SCANCODE_J]);
+        B += 0.1*turbo*(keyboard[SDL_SCANCODE_I] - keyboard[SDL_SCANCODE_K]);
+        C += 0.1*turbo*(keyboard[SDL_SCANCODE_O] - keyboard[SDL_SCANCODE_L]);
 
-          if (e.button.button == SDL_BUTTON_MIDDLE)
-          {
-            c.v.x = 0;
-            c.v.y = 0;
-            c.v.z = 0;
-          }
-        }
-        else if (e.type == SDL_MOUSEBUTTONUP)
-        {
-          if (e.button.button == SDL_BUTTON_LEFT)
-          {
-            c.v.x = 0;
-            c.v.y = 0;
-            c.v.z = 0;
-          }
-          if (e.button.button == SDL_BUTTON_RIGHT)
-          {
-            c.v.x = 0;
-            c.v.y = 0;
-            c.v.z = 0;
-          }
-        }
-        else if (e.type == SDL_MOUSEMOTION)
-        {
-          int dTheta, dPhi;
-          SDL_GetRelativeMouseState(&dTheta, &dPhi);
-          c.theta += dTheta / (100 * PI);
-          c.phi -= dPhi / (100 * PI);
+        dt *= 1 + turbo*0.01*(keyboard[SDL_SCANCODE_T] - keyboard[SDL_SCANCODE_G]);
 
-          if (c.phi > PI / 2)
-          {
-            c.phi = PI / 2;
-          }
-          if (c.phi < -PI / 2)
-          {
-            c.phi = -PI / 2;
-          }
-        }
-      }
+        POV *= 1 + turbo*0.01*(keyboard[SDL_SCANCODE_X] - keyboard[SDL_SCANCODE_Z]);
 
-      //move camera
-      if (flyFree)
-      {
-        c.v = scaleParticle(lorenzeVelocityFromPosition(c.p), !paused * timeSlow);
-        particleToSphere(c.v, &c.theta, &c.phi);
-        stepParticle(&c.p, c.v);
-      }
-      else
-      {
-        stepParticle(&c.p, c.v);
-      }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
       
-      //move particles
-      for (int i = 0; i < numParticles; i++)
-      {
-        lorenzStepParticle(&particleList[i]);
-      }
+        for (int i = 0; i < numParticles; i++)
+        {
+            V3 p = particals[i];
+            V3 v = {
+                A * (p.y - p.x),
+                p.x * (C - p.z) - p.y,
+                p.x * p.y - B * p.z
+            };
+            p = add(p,mult(dt*stopTime,v));
+            particals[i] = p;
 
-      //clear screen
-      SDL_SetRenderDrawColor(c.renderer, 0x00, 0x00, 0x00, 0xFF);
-      SDL_RenderClear(c.renderer);
+            p = add(p, mult(-1,c));
 
-      //draw particles
-      SDL_SetRenderDrawColor(c.renderer, 255, 255, 255, 255);
-      for (int i = 0; i < numParticles; i++)
-      {
-        drawParticle(c, particleList[i]);
-      }
+            V3 r = {
+                p.x*cosTheta - p.z*sinTheta,
+               -p.x*sinPhi*sinTheta + p.y*cosPhi - p.z*sinPhi*cosTheta,
+               p.x*cosPhi*sinTheta + p.y*sinPhi + p.z*cosPhi*cosTheta
+            };
 
-      //Update screen
-      SDL_RenderPresent(c.renderer);
+            if (isinf(r.x) || isinf(r.y) || isinf(r.z) ||
+                isnan(r.x) || isnan(r.y) || isnan(r.z))
+            particals[i] = r = randV3();
 
-      //frame cap
-      if (SDL_GetTicks() - startOfFrame < dt * 1000)
-      {
-        SDL_Delay(dt * 1000 - (SDL_GetTicks() - startOfFrame));
-        startOfFrame = SDL_GetTicks();
-      }
+            else if (r.z > 0)
+            {
+                points[i].x = w / 2 * (1 + POV *  r.x / r.z);
+                points[i].y = h / 2 * (1 + POV * -r.y / r.z);
+            }
+        }
+        
+        SDL_RenderDrawPointsF(renderer, points, numParticles);
+        SDL_RenderPresent(renderer);
     }
-    free(particleList);
-  }
-  closeCamera(&c);
-  return 0;
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);  //closes window
+    SDL_Quit();
+    return 0;
 }
